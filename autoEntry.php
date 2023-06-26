69696ef9a9ddbedeabf95ebaf1367938c6fe27f5
karishmamalek <karimalik26997@gmail.com>
2023-06-26 15:02:56 +0530

    Updated time of message_entry table

--- autoEntry.php
+++ autoEntry.php
@@ -2,6 +2,7 @@
 include_once 'messageEntryClass.php';
 $message_entry = new messageEntryClass();
 $message = rand(11111, 99999)."abcdxyz";
-$time = date("h:i:s");
+// $time = date("h:i:s");
+$time = date('Y-m-d H:i:s');
 $message_entry->addMessageEntry($message, $time);
 ?>
